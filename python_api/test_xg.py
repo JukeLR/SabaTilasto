6aafe53699b1d3d1d83c2deec60257723aae4bbb
JukeLR <jukka.latvaranta@gmail.com>
2025-12-11 16:54:39 +0200

    Poista python_api kansio ja lisää .gitignore

--- python_api/test_xg.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-data = [
-    {"x": 0.5, "y": 0.7, "team": "home", "type": "Wrist", "player_id": 12, "games_id": 88}
-]
-resp = requests.post("http://localhost:8000/calculate_xg", json=data)
-print("Response:", resp.text)
